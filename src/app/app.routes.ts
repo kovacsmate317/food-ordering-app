7fedee3c06c73826b6fc4d89554a190e7799af16
kovacsmate317@gmail.com <kovacsmate317>
2025-03-17 23:23:55 +0100

    basic folder structure, basic pages without explicit template

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { CartComponent } from './pages/cart/cart.component';
+import { MenuComponent } from './pages/menu/menu.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'cart', component: CartComponent },
+  { path: 'menu', component: MenuComponent },
+
+  { path: '**', component: HomeComponent },
+];
