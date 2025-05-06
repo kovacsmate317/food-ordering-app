0b5adfaa894be500084a2ec157447aedfcb8c1eb
kovacsmate317 <kovacsmate317@gmail.com>
2025-05-06 23:59:09 +0200

    firebase login, config, authSerivce

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,10 +2,25 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
+    provideFirebaseApp(() =>
+      initializeApp({
+        projectId: 'food-ordering-app-72391',
+        appId: '1:484434102:web:04bc81f19c8ae73abec617',
+        storageBucket: 'food-ordering-app-72391.firebasestorage.app',
+        apiKey: 'AIzaSyDlNDvRPfp7GViF_vpS2wpu_4JAeQ2pE6E',
+        authDomain: 'food-ordering-app-72391.firebaseapp.com',
+        messagingSenderId: '484434102',
+      })
+    ),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
   ],
 };
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,42 +1,73 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule, NgForm } from '@angular/forms';
-import { RouterModule } from '@angular/router';
+import {
+  FormControl,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { Router, RouterModule } from '@angular/router';
+import { AuthService } from '../../shared/services/auth.service';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule, FormsModule, RouterModule],
+  imports: [CommonModule, FormsModule, RouterModule, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent {
-  email = '';
-  password = '';
-  submitted = false;
+  email = new FormControl('', [Validators.required, Validators.email]);
+  password = new FormControl('', [
+    Validators.required,
+    Validators.minLength(6),
+  ]);
+  isLoading: boolean = false;
+  errorMessage: string = '';
 
-  onSubmit(form: NgForm) {
-    this.submitted = true;
-    if (form.invalid) return;
+  constructor(private authService: AuthService, private router: Router) {}
 
-    console.log('Login submitted:', form.value);
-  }
-
-  getErrorMessage(form: NgForm): string {
-    const email = this.email.trim();
-    const password = this.password.trim();
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-
-    if (form.controls['email']?.invalid && this.submitted) {
-      if (!emailRegex.test(email)) {
-        return 'Please enter a valid email address.';
-      }
+  onLogin() {
+    if (this.email.invalid) {
+      this.errorMessage = 'Invalid email';
+    }
+    if (this.password.invalid) {
+      this.errorMessage = 'Invalid password';
     }
 
-    if (form.controls['password']?.invalid && this.submitted) {
-      return 'Password is required.';
-    }
+    const emailValue = this.email.value || '';
+    const passwordValue = this.password.value || '';
 
-    return '';
+    this.isLoading = true;
+    //this.showLoginForm = false;
+    this.errorMessage = '';
+
+    this.authService
+      .signIn(emailValue, passwordValue)
+      .then((userCredential) => {
+        console.log('Login successful:', userCredential.user);
+        this.authService.updateLoginStatus(true);
+        this.router.navigateByUrl('/home');
+      })
+      .catch((error) => {
+        console.error('Login error:', error);
+        this.isLoading = false;
+        //this.showLoginForm = true;
+
+        switch (error.code) {
+          case 'auth/user-not-found':
+            this.errorMessage = 'No account found with this email address';
+            break;
+          case 'auth/wrong-password':
+            this.errorMessage = 'Incorrect password';
+            break;
+          case 'auth/invalid-credential':
+            this.errorMessage = 'Invalid email or password';
+            break;
+          default:
+            this.errorMessage =
+              'Authentication failed. Please try again later.';
+        }
+      });
   }
 }
